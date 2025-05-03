5c1a0c59f53660cd344ada64e0fb42cf7cc85cba
tianxiu2b2t <atianxiua@163.com>
2025-05-03 21:57:01 +0800

    feat: 管理员的操作

---
+++ vcmp/functions/admin.py
@@ -0,0 +1,19 @@
+from ..__export import funcs
+
+def ban_ip(
+    address: str
+):
+    """Ban an IP address."""
+    funcs.ban_ip(address)
+
+def pardon_ip(
+    address: str
+):
+    """Pardon an IP address."""
+    funcs.unban_ip(address)
+
+def ip_banned(
+    address: str
+) -> bool:
+    """Check if an IP address is banned."""
+    return funcs.is_ip_banned(address)
