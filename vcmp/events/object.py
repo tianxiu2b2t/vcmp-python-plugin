73eab8747775647ffaa26f255845b28ec58a778a
xiaoyuan-SQ <fengxis94585@163.com>
2025-05-03 21:52:38 +0800

    add: 给事件一些完善 (#1)

--- vcmp/events/object.py
+++ vcmp/events/object.py
@@ -4,7 +4,19 @@ class ObjectEvent(Event):
     ...
 
 class ObjectShotEvent(ObjectEvent):
-    ...
+    __fields__ = (
+        "objectid",
+        "id",
+        "weaponid",
+    )
+    objectid: int
+    id: int
+    weaponid: int
 
 class ObjectTouchedEvent(ObjectEvent):
-    ...
+    __fields__ = (
+        "objectid",
+        "id",
+    )
+    objectid: int
+    id: int
--- vcmp/events/vehicle.py
+++ vcmp/events/vehicle.py
@@ -1,13 +1,25 @@
+from vcmp.__export import vcmpVehicleUpdate
 from .abc import Event
 
 class VehicleEvent(Event):
     ...
 
 class VehicleUpdateEvent(VehicleEvent):
-    ...
+    __fields__ = (
+        "vehicleid",
+        "updatetype",
+    )
+    vehicleid: int
+    updatetype: vcmpVehicleUpdate
 
 class VehicleExplodeEvent(VehicleEvent):
-    ...
+    __fields__ = (
+        "vehicleid",
+    )
+    vehicleid: int
 
 class VehicleRespawnEvent(VehicleEvent):
-    ...
+    __fields__ = (
+        "vehicleid",
+    )
+    vehicleid: int
