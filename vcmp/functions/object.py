038e094630f58b7ea8c718deb508baee97413e47
tianxiu2b2t <atianxiua@163.com>
2025-05-04 21:08:33 +0800

    add: 添加创建 Object

---
+++ vcmp/functions/object.py
@@ -0,0 +1,21 @@
+from ..__export import funcs
+from ..vec import Vector
+from ..instance import Object
+
+def create_object(
+    model: int,
+    world: int,
+    pos: Vector,
+    alpha: int
+):
+    """
+
+    Creates an object.
+
+    :param model: The model of the object.
+    :param world: The world to create the object in.
+    :param pos: The position of the object.
+    :param alpha: The alpha of the object.
+    :return: The object created.
+    """
+    return Object(funcs.create_object(model, world, pos.x, pos.y, pos.z, alpha))
