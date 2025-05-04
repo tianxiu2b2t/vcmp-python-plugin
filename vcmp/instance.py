5ccb89e70489100ed04ba8432193e33091928210
tianxiu2b2t <atianxiua@163.com>
2025-05-04 20:46:08 +0800

    add: 添加 Checkpoint

--- vcmp/instance.py
+++ vcmp/instance.py
@@ -1336,9 +1336,85 @@ class Pickup:
             _pickups.append(pickup)
         return pickup
 
+
+class CheckPoint:
+    def __init__(self, id: int):
+        self._id = id
+
+    @property
+    def id(self) -> int:
+        return self._id
+
+    @property
+    def sphere(self):
+        return funcs.is_check_point_sphere(self._id)
+    
+    @property
+    def world(self):
+        return funcs.get_check_point_world(self._id)
+    
+    @world.setter
+    def world(self, world: int):
+        funcs.set_check_point_world(self._id, world)
+
+    @property
+    def position(self):
+        return Vector(**funcs.get_check_point_position(self._id))
+
+    @position.setter
+    def position(self, position: Vector):
+        funcs.set_check_point_position(self._id, position.x, position.y, position.z)
+
+    @property
+    def color(self) -> RGB:
+        return RGB(**funcs.get_check_point_colour(self._id))
+
+    @color.setter
+    def color(self, color: RGB):
+        funcs.set_check_point_colour(self._id, color.red, color.green, color.blue, color.alpha)
+
+    @property
+    def radius(self):
+        return funcs.get_check_point_radius(self._id)
+
+    @radius.setter
+    def radius(self, radius: float):
+        funcs.set_check_point_radius(self._id, radius)
+
+    @property
+    def owner(self):
+        return get_player_from_id(funcs.get_check_point_owner(self._id))
+
+    def delete(self):
+        """
+        Delete the pickup
+        """
+        if not funcs.check_entity_exists(vcmpEntityPool.vcmpEntityPoolCheckPoint, self._id):
+            return
+        funcs.delete_check_point(self._id)
+        _checkpoints.remove(self)
+
+    def is_streamed_for_player(self, player: int | Player) -> bool:
+        id = player if isinstance(player, int) else player.id
+        return funcs.is_check_point_streamed_for_player(self._id, id)
+
+    def __del__(self):
+        self.delete()
+
+    def __new__(cls, pickup_id: int):
+        if not funcs.check_entity_exists(vcmpEntityPool.vcmpEntityPoolCheckPoint, pickup_id):
+            return None
+        
+        checkpoint = next((checkpoint for checkpoint in _checkpoints if checkpoint.id == pickup_id), None)
+        if checkpoint is None:
+            checkpoint = super().__new__(cls)
+            _checkpoints.append(checkpoint)
+        return checkpoint
+
 _players: list[Player] = []
 _vehicles: list[Vehicle] = []
 _pickups: list[Pickup] = []
+_checkpoints: list[CheckPoint] = []
 _pools_max: dict[vcmpEntityPool, int] = {
     vcmpEntityPool.vcmpEntityPoolVehicle: constant.MAX_VEHICLES,
     vcmpEntityPool.vcmpEntityPoolPickup: constant.MAX_PICKUPS,
