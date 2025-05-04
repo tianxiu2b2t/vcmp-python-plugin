da702d47c7c23693c18efe46031a59f63905fa47
tianxiu2b2t <atianxiua@163.com>
2025-05-04 20:56:58 +0800

    add: 添加创建 Pickup 函数

---
+++ vcmp/functions/pickup.py
@@ -0,0 +1,25 @@
+from ..__export import funcs
+from ..vec import Vector
+from ..instance import Pickup
+
+def create_pickup(
+    model: int,
+    world: int,
+    quantity: int,
+    pos: Vector,
+    alpha: int,
+    automatic: bool
+):
+    """
+    Creates a pickup.
+
+    :param model: The model of the pickup.
+    :param world: The world the pickup will be in.
+    :param quantity: The quantity of the pickup.
+    :param pos: The position of the pickup.
+    :param alpha: The alpha of the pickup.
+    :param automatic: Whether the pickup is automatic or not.
+    :return: The pickup ID.
+    """
+    id = funcs.create_pickup(model, world, quantity, pos.x, pos.y, pos.z, alpha, automatic)
+    return Pickup(id)
