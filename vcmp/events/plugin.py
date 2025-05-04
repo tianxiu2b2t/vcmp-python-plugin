2d4eccc7354e29a7db60cda9ac3d50e168f79414
tianxiu2b2t <atianxiua@163.com>
2025-05-04 18:02:32 +0800

    add: 增加对 PluginCommandEvent 事件

---
+++ vcmp/events/plugin.py
@@ -0,0 +1,12 @@
+from .abc import Event
+
+class PluginEvent(Event):
+    ...
+
+class PluginCommnadEvent(PluginEvent):
+    __fields__ = (
+        "command_identifier",
+        "message"
+    )
+    command_identifier: str
+    message: str
