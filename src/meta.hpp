4a081fe2abc5f4d626ac88869c5ce4c050ff012c
tianxiu2b2t <atianxiua@163.com>
2025-05-04 23:42:05 +0800

    chore: 插件版本

--- src/meta.hpp
+++ src/meta.hpp
@@ -1,8 +1,8 @@
 #ifndef VCMPPLUGIN_TX_META
 #define VCMPPLUGIN_TX_META
-#define PLUGIN_MAJOR 1
+#define PLUGIN_MAJOR 0
 #define PLUGIN_MINOR 0
-#define PLUGIN_PATCH 0
+#define PLUGIN_PATCH 1
 #define PLUGIN_NAME "vcmp-python-plugin"
-#define PLUGIN_VERSION "1.0.0"
+#define PLUGIN_VERSION "0.0.1"
 #endif
