1936bc389da5c3ce2cf78db00af7db8ad1b195aa
tianxiu2b2t <atianxiua@163.com>
2025-05-05 10:07:41 +0800

    chore: 测试构建

--- src/meta.hpp
+++ src/meta.hpp
@@ -2,7 +2,7 @@
 #define VCMPPLUGIN_TX_META
 #define PLUGIN_MAJOR 0
 #define PLUGIN_MINOR 0
-#define PLUGIN_PATCH 5
+#define PLUGIN_PATCH 6
 #define PLUGIN_NAME "vcmp-python-plugin"
-#define PLUGIN_VERSION "0.0.5"
+#define PLUGIN_VERSION "0.0.6"
 #endif
