96692354ae1111e8e0e54aa379497b64ee01a294
tianxiu2b2t <atianxiua@163.com>
2025-05-17 07:31:57 +0800

    feat: 给个判断玩家是否已连接

---
+++ vcmp/functions/player.py
@@ -0,0 +1,14 @@
+from ..__export import funcs
+
+def is_player_connected(
+    id: int
+) -> bool:
+    """Checks if a player is connected to the server.
+
+    Args:
+        id (int): The ID of the player.
+
+    Returns:
+        bool: True if the player is connected, False otherwise.
+    """
+    return funcs.is_player_connected(id)
