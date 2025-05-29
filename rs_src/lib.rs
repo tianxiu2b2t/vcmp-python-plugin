5cae358f03e5822482fca33a8d81c3d7f3897d4c
shenjackyuanjie <3695888@qq.com>
2025-05-30 00:18:18 +0800

    size of

--- rs_src/lib.rs
+++ rs_src/lib.rs
@@ -2,6 +2,9 @@ use bindings::raw::{PluginCallbacks, PluginFuncs, PluginInfo};
 
 pub mod bindings;
 
+
+pub const PLUGIN_VERSION: u32 = 1;
+
 /// 插件入口点
 ///
 /// 好消息: 反正也没别的需要 extern 的函数了
@@ -21,11 +24,20 @@ extern "C" fn VcmpPluginInit(plugin_functions: *mut PluginFuncs, plugin_callback
 
         // 参考 cpp.ing
         info.apiMajorVersion = bindings::API_MAJOR as u16;
-        info.apiMinorVersion = bindings::API_MINOR as u16;
-        info.pluginVersion = 1;
+        // info.apiMinorVersion = bindings::API_MINOR as u16 - 1; // 难蚌 compat
+        info.apiMinorVersion = 0;
+        info.pluginVersion = PLUGIN_VERSION;
 
-        println!("vcmp-plugin-rs id: {}", info.pluginId);
-        
+        println!("vcmp-plugin-rs info: {:#?}", info);
+        println!("vcmp-plugin-rs callback: {:#?}", callbacks);
+        // println!("vcmp-plugin-rs functions: {:?}", functions);
+
+        println!("sizeof callback: {}", std::mem::size_of::<PluginCallbacks>());
+        println!("sizeof functions: {}", std::mem::size_of::<PluginFuncs>());
+
+        println!("give sizeof callback: {}", callbacks.structSize);
+        println!("give sizeof functions: {}", functions.structSize)
+
     }
 
     println!("vcmp-plugin-rs loaded");
