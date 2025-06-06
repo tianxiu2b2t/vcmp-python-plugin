153edce53452686c7f0153ea8576c2a3d55902e1
tianxiu2b2t <atianxiua@163.com>
2025-06-06 17:15:19 +0800

    feat: 结构体

---
+++ crates/vcmp_bindings/src/util_struct.rs
@@ -0,0 +1,110 @@
+pub struct RGB {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
+}
+
+impl Color {
+    pub fn from_rgba(value: u32) -> Self {
+        Self {
+            r: (value >> 24) as u8,
+            g: (value >> 16) as u8,
+            b: (value >> 8) as u8,
+            a: value as u8,
+        }
+    }
+    pub fn from_argb(value: u32) -> Self {
+        Self {
+            a: (value >> 24) as u8,
+            r: (value >> 16) as u8,
+            g: (value >> 8) as u8,
+            b: value as u8,
+        }
+    }
+    pub fn to_rgba(&self) -> u32 {
+        ((self.r as u32) << 24) | ((self.g as u32) << 16) | ((self.b as u32) << 8) | (self.a as u32)
+    }
+    pub fn to_argb(&self) -> u32 {
+        ((self.a as u32) << 24) | ((self.r as u32) << 16) | ((self.g as u32) << 8) | (self.b as u32)
+    }
+}
+
+pub struct ARGBColor(pub Color);
+
+impl From<Color> for ArgbColor {
+    fn from(value: Color) -> Self {
+        Self(value)
+    }
+}
+
+impl Into<u32> for ArgbColor {
+    fn into(self) -> u32 {
+        self.0.to_argb()
+    }
+}
+
+pub struct RgbaColor(pub Color);
+
+impl From<Color> for RgbaColor {
+    fn from(value: Color) -> Self {
+        Self(value)
+    }
+}
+
+impl Into<u32> for RgbaColor {
+    fn into(self) -> u32 {
+        self.0.to_rgba()
+    }
+}
+
+impl Display for Color {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Color({},{},{},{})", self.r, self.g, self.b, self.a)
+    }
+}
+
+pub struct Vector {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+impl Vector {
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        Self { x, y, z }
+    }
+
+    pub fn distance_from(&self, other: &Self) -> f32 {
+        let dx = self.x - other.x;
+        let dy = self.y - other.y;
+        (dx * dx + dy * dy).sqrt()
+    }
+    pub fn length(&self) -> f32 {
+        (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
+    }
+}
+
+impl Display for Vector {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Vector({}, {}, {})", self.x, self.y, self.z)
+    }
+}
+
+pub struct Quaternion {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+    pub w: f32,
+}
+impl Quaternion {
+    pub fn new(x: f32, y: f32, z: f32, w: f32) -> Self {
+        Self { x, y, z, w }
+    }
+}
+
+impl Display for Quaternion {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Quaternion({}, {}, {}, {})", self.x, self.y, self.z, self.w)
+    }
+}
