d8695d7aa963b734bb6cc0fa06fae288ee3cd323
tianxiu2b2t <atianxiua@163.com>
2025-06-08 17:52:59 +0800

    refactor: 把 server 相关的拆过去

--- crates/vcmp_bindings/src/func.rs
+++ crates/vcmp_bindings/src/func.rs
@@ -42,51 +42,7 @@ impl VcmpFunctions {
         std::mem::size_of::<PluginFuncs>()
     }
 
-    /// 获取服务器版本号
-    pub fn server_version(&self) -> u32 {
-        (self.inner.GetServerVersion)()
-    }
-
-    /// 获取服务器设置
-    pub fn get_server_settings(&self) -> VcmpServerSettings {
-        let mut setting = VcmpServerSettings::new_empty();
-        let setting_ptr = setting.inner_mut_ptr();
-        let _ = (self.inner.GetServerSettings)(setting_ptr);
-        setting
-    }
-
-    pub fn get_plugin_count(&self) -> u32 {
-        (self.inner.GetNumberOfPlugins)()
-    }
-
-    pub fn get_plugin_info(&self, plugin_id: i32) -> VcmpResult<VcmpPluginInfo> {
-        let mut info = VcmpPluginInfo::new_empty();
-        let info_ptr = info.inner_mut_ptr();
-        let code = (self.inner.GetPluginInfo)(plugin_id, info_ptr);
-        if code != 0 {
-            Err(VcmpError::from(code))
-        } else {
-            Ok(info)
-        }
-    }
-
-    pub fn find_plugin(&self, plugin_name: &str) -> Option<i32> {
-        let ptr = plugin_name.as_ptr() as *const i8;
-        let res = (self.inner.FindPlugin)(ptr);
-        if res == -1 { None } else { Some(res) }
-    }
-
-    /// TODO
-    pub fn send_plugin_command(&self, command_identifier: i32, command: &str) -> VcmpResult<()> {
-        let cmd_ptr = command.as_ptr() as *const i8;
-        let code = (self.inner.SendPluginCommand)(command_identifier as u32, cmd_ptr);
-        if code != 0 {
-            Err(VcmpError::from(code))
-        } else {
-            Ok(())
-        }
-    }
-
+    /// 全局性的 last error
     pub fn get_last_error(&self) -> VcmpError {
         VcmpError::from((self.inner.GetLastError)())
     }
---
+++ crates/vcmp_bindings/src/func/server.rs
@@ -0,0 +1,87 @@
+use crate::func::VcmpFunctions;
+use crate::utils::{Quaternion, Vector};
+use crate::{VcmpError, VcmpResult};
+
+pub trait QueryServer {
+    fn server_version() -> u32;
+}
+pub trait SetServer {
+}
+
+impl SetServer for VcmpFunctions {
+    fn set_server_name(&self, name: &str) -> VcmpResult<()> {
+        let name = encode_to_gbk(name);
+        let name_ptr = name.as_ptr() as *const i8;
+        let code = (self.inner.SetServerName)(name_ptr);
+        if code != 0 {
+            Err(VcmpError::from(code))
+        } else {
+            Ok(())
+        }
+    }
+    fn set_server_password(&self, password: &str) -> VcmpResult<()> {
+        let password = encode_to_gbk(password);
+        let password_ptr = password.as_ptr() as *const i8;
+        let code = (self.inner.SetServerPassword)(password_ptr);
+        if code != 0 {
+            Err(VcmpError::from(code))
+        } else {
+            Ok(())
+        }
+    }
+    fn set_gamemode(&self, gamemode: &str) -> VcmpResult<()> {
+        let mut gamemode = encode_to_gbk(gamemode).to_vec();
+        gamemode.push(0);
+        //println!("setting gamemode to {gamemode:?}");
+        let gamemode_ptr = gamemode.as_ptr() as *const i8;
+        let code = (self.inner.SetGameModeText)(gamemode_ptr);
+        if code != 0 {
+            Err(VcmpError::from(code))
+        } else {
+            Ok(())
+        }
+    }
+    fn get_max_players(&self) -> u32 {
+        (self.inner.GetMaxPlayers)()
+    }
+    fn shutdown(&self) {
+        (self.inner.ShutdownServer)()
+    }
+}
+
+impl QueryServer for VcmpFunctions {
+    /// 获取服务器版本
+    fn server_version(&self) -> u32 {
+        (self.inner.GetServerVersion)()
+    }
+    /// 获取服务器设置
+    fn server_settings() -> ServerSettings {
+        let mut setting = VcmpServerSettings::new_empty();
+        let setting_ptr = setting.inner_mut_ptr();
+        let _ = (self.inner.GetServerSettings)(setting_ptr);
+        setting
+    }
+    fn get_server_name(&self) -> String {
+        self.server_settings().server_name()
+    }
+    fn get_server_password(&self) -> String {
+        let buf = vec![0u8; 1024];
+        let buf_ptr = buf.as_ptr() as *mut i8;
+        let _ = (self.inner.GetServerPassword)(buf_ptr, 1024);
+        decode_gbk(&buf)
+    }
+    fn get_gamemode(&self) -> String {
+        let buf = vec![0u8; 1024];
+        let buf_ptr = buf.as_ptr() as *mut i8;
+        let _ = (self.inner.GetGameModeText)(buf_ptr, 1024);
+        decode_gbk(&buf)
+    }
+    fn set_max_players(&self, max_player: u32) -> VcmpResult<()> {
+        let code = (self.inner.SetMaxPlayers)(max_player);
+        if code != 0 {
+            Err(VcmpError::from(code))
+        } else {
+            Ok(())
+        }
+    }
+}
