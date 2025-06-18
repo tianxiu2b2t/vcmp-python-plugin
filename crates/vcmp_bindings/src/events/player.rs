6eae1269a9c94f9192419ac1c0a871ddb60363c7
tianxiu2b2t <atianxiua@163.com>
2025-06-18 21:27:11 +0800

    feat: 完善 player 事件

--- crates/vcmp_bindings/src/events/player.rs
+++ crates/vcmp_bindings/src/events/player.rs
@@ -1,5 +1,5 @@
 use std::{
-    ffi::{c_char, CString}, fmt::Display, str::FromStr
+    ffi::{c_char, CString}, str::FromStr
 };
 
 pub struct IncomingConnectionEvent {
@@ -53,7 +53,7 @@ impl From<(*mut c_char, usize, *const c_char, *const c_char)> for IncomingConnec
         }
     }
 }
-#[derive(Debug, Clone)]
+
 pub struct ClientScriptDataEvent {
     pub player_id: i32,
     pub data: Vec<u8>,
@@ -70,8 +70,404 @@ impl From<(i32, *const u8, usize)> for ClientScriptDataEvent {
     }
 }
 
-impl Display for ClientScriptDataEvent {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "ClientScriptDataEvent {{ player_id: {}, data: {:?} }}", self.player_id, self.data)
+pub struct PlayerConnectEvent {
+    pub player_id: i32,
+}
+
+impl From<i32> for PlayerConnectEvent {
+    fn from(value: i32) -> Self {
+        Self { player_id: value }
+    }
+}
+
+pub struct PlayerDisconnectEvent {
+    pub player_id: i32,
+    pub reason: i32,
+}
+
+impl From<(i32, i32)> for PlayerDisconnectEvent {
+    fn from(value: (i32, i32)) -> Self {
+        Self {
+            player_id: value.0,
+            reason: value.1,
+        }
+    }
+}
+
+pub struct PlayerRequestClassEvent {
+    pub player_id: i32,
+    pub class_id: i32,
+}
+
+impl From<(i32, i32)> for PlayerRequestClassEvent {
+    fn from(value: (i32, i32)) -> Self {
+        Self {
+            player_id: value.0,
+            class_id: value.1,
+        }
+    }
+}
+
+pub struct PlayerSpawnEvent {
+    pub player_id: i32,
+}
+
+impl From<i32> for PlayerSpawnEvent {
+    fn from(value: i32) -> Self {
+        Self { player_id: value }
+    }
+}
+
+pub struct PlayerRequestSpawn {
+    pub player_id: i32,
+}
+
+impl From<i32> for PlayerRequestSpawn {
+    fn from(value: i32) -> Self {
+        Self { player_id: value }
+    }
+}
+
+pub struct PlayerDeathEvent {
+    pub player_id: i32,
+    pub killer_id: i32,
+    pub reason: i32,
+    pub body: i32
+} 
+
+impl From<(i32, i32, i32, i32)> for PlayerDeathEvent {
+    fn from(value: (i32, i32, i32, i32)) -> Self {
+        Self {
+            player_id: value.0,
+            killer_id: value.1,
+            reason: value.2,
+            body: value.3
+        }
+    }
+}
+
+pub struct PlayerUpdateEvent {
+    pub player_id: i32,
+    pub update: i32,
+}
+
+impl From<(i32, i32)> for PlayerUpdateEvent {
+    fn from(value: (i32, i32)) -> Self {
+        Self {
+            player_id: value.0,
+            update: value.1,
+        }
+    }
+}
+
+pub struct PlayerRequestEnterVehicleEvent {
+    pub player_id: i32,
+    pub vehicle_id: i32,
+    pub slot_index: i32,
+}
+
+impl From<(i32, i32, i32)> for PlayerRequestEnterVehicleEvent {
+    fn from(value: (i32, i32, i32)) -> Self {
+        Self {
+            player_id: value.0,
+            vehicle_id: value.1,
+            slot_index: value.2,
+        }
+    }
+}
+
+pub struct PlayerEnterVehicleEvent {
+    pub player_id: i32,
+    pub vehicle_id: i32,
+    pub slot_index: i32,
+}
+
+impl From<(i32, i32, i32)> for PlayerEnterVehicleEvent {
+    fn from(value: (i32, i32, i32)) -> Self {
+        Self {
+            player_id: value.0,
+            vehicle_id: value.1,
+            slot_index: value.2,
+        }
+    }
+}
+
+pub struct PlayerExitVehicleEvent {
+    pub player_id: i32,
+    pub vehicle_id: i32,
+}
+
+impl From<(i32, i32)> for PlayerExitVehicleEvent {
+    fn from(value: (i32, i32)) -> Self {
+        Self {
+            player_id: value.0,
+            vehicle_id: value.1,
+        }
+    }
+}
+
+pub struct PlayerNameChangeEvent {
+    pub player_id: i32,
+    pub old_name: String,
+    pub new_name: String,
+}
+
+impl From<(i32, *const c_char, *const c_char)> for PlayerNameChangeEvent {
+    fn from(value: (i32, *const c_char, *const c_char)) -> Self {
+        unsafe {
+            Self {
+                player_id: value.0,
+                old_name: std::ffi::CStr::from_ptr(value.1)
+                    .to_string_lossy()
+                    .to_string(),
+                new_name: std::ffi::CStr::from_ptr(value.2)
+                    .to_string_lossy()
+                    .to_string(),
+            }
+        }
+    }
+}
+
+pub struct PlayerStateChangeEvent {
+    pub player_id: i32,
+    pub old_state: i32,
+    pub new_state: i32,
+}
+
+impl From<(i32, i32, i32)> for PlayerStateChangeEvent {
+    fn from(value: (i32, i32, i32)) -> Self {
+        Self {
+            player_id: value.0,
+            old_state: value.1,
+            new_state: value.2,
+        }
+    }
+}
+
+pub struct PlayerActionChangeEvent {
+    pub player_id: i32,
+    pub old_action: i32,
+    pub new_action: i32,
+}
+
+impl From<(i32, i32, i32)> for PlayerActionChangeEvent {
+    fn from(value: (i32, i32, i32)) -> Self {
+        Self {
+            player_id: value.0,
+            old_action: value.1,
+            new_action: value.2,
+        }
+    }
+}
+
+pub struct PlayerOnFireChangeEvent {
+    pub player_id: i32,
+    pub is_on_fire: bool,
+}
+
+impl From<(i32, u8)> for PlayerOnFireChangeEvent {
+    fn from(value: (i32, u8)) -> Self {
+        Self {
+            player_id: value.0,
+            is_on_fire: value.1 != 0,
+        }
+    }
+}
+
+pub struct PlayerCrouchChangeEvent {
+    pub player_id: i32,
+    pub is_crouching: bool,
+}
+
+impl From<(i32, u8)> for PlayerCrouchChangeEvent {
+    fn from(value: (i32, u8)) -> Self {
+        Self {
+            player_id: value.0,
+            is_crouching: value.1 != 0,
+        }
+    }
+}
+
+pub struct PlayerGameKeysChangeEvent {
+    pub player_id: i32,
+    pub old_keys: u32,
+    pub new_keys: u32,
+}
+
+impl From<(i32, u32, u32)> for PlayerGameKeysChangeEvent {
+    fn from(value: (i32, u32, u32)) -> Self {
+        Self {
+            player_id: value.0,
+            old_keys: value.1,
+            new_keys: value.2,
+        }
+    }
+}
+
+pub struct PlayerBeginTypingEvent {
+    pub player_id: i32,
+}
+
+impl From<i32> for PlayerBeginTypingEvent {
+    fn from(value: i32) -> Self {
+        Self { player_id: value }
+    }
+}
+
+pub struct PlayerEndTypingEvent {
+    pub player_id: i32,
+}
+
+impl From<i32> for PlayerEndTypingEvent {
+    fn from(value: i32) -> Self {
+        Self { player_id: value }
+    }
+}
+
+pub struct PlayerAwayChangeEvent {
+    pub player_id: i32,
+    pub is_away: bool,
+}
+
+impl From<(i32, u8)> for PlayerAwayChangeEvent {
+    fn from(value: (i32, u8)) -> Self {
+        Self {
+            player_id: value.0,
+            is_away: value.1 != 0,
+        }
+    }
+}
+
+pub struct PlayerMessageEvent {
+    pub player_id: i32,
+    pub message: String,
+}
+
+impl From<(i32, *const c_char)> for PlayerMessageEvent {
+    fn from(value: (i32, *const c_char)) -> Self {
+        unsafe {
+            Self {
+                player_id: value.0,
+                message: std::ffi::CStr::from_ptr(value.1)
+                    .to_string_lossy()
+                    .to_string(),
+            }
+        }
+    }
+}
+
+pub struct PlayerCommandEvent {
+    pub player_id: i32,
+    pub command: String,
+}
+
+impl From<(i32, *const c_char)> for PlayerCommandEvent {
+    fn from(value: (i32, *const c_char)) -> Self {
+        unsafe {
+            Self {
+                player_id: value.0,
+                command: std::ffi::CStr::from_ptr(value.1)
+                    .to_string_lossy()
+                    .to_string(),
+            }
+        }
+    }
+}
+
+pub struct PlayerPrivateMessageEvent {
+    pub player_id: i32,
+    pub target_player_id: i32,
+    pub message: String,
+}
+
+impl From<(i32, i32, *const c_char)> for PlayerPrivateMessageEvent {
+    fn from(value: (i32, i32, *const c_char)) -> Self {
+        unsafe {
+            Self {
+                player_id: value.0,
+                target_player_id: value.1,
+                message: std::ffi::CStr::from_ptr(value.2)
+                    .to_string_lossy()
+                    .to_string(),
+            }
+        }
+    }
+}
+
+pub struct PlayerKeyBindDownEvent {
+    pub player_id: i32,
+    pub bind_id: i32,
+}
+
+impl From<(i32, i32)> for PlayerKeyBindDownEvent {
+    fn from(value: (i32, i32)) -> Self {
+        Self {
+            player_id: value.0,
+            bind_id: value.1,
+        }
+    }
+}
+
+pub struct PlayerKeyBindUpEvent {
+    pub player_id: i32,
+    pub bind_id: i32,
+}
+
+impl From<(i32, i32)> for PlayerKeyBindUpEvent {
+    fn from(value: (i32, i32)) -> Self {
+        Self {
+            player_id: value.0,
+            bind_id: value.1,
+        }
+    }
+}
+
+pub struct PlayerSpectateEvent {
+    pub player_id: i32,
+    pub target_player_id: i32,
+}
+
+impl From<(i32, i32)> for PlayerSpectateEvent {
+    fn from(value: (i32, i32)) -> Self {
+        Self {
+            player_id: value.0,
+            target_player_id: value.1,
+        }
+    }
+}
+
+pub struct PlayerCrashReportEvent {
+    pub player_id: i32,
+    pub report: String,
+}
+
+impl From<(i32, *const c_char)> for PlayerCrashReportEvent {
+    fn from(value: (i32, *const c_char)) -> Self {
+        unsafe {
+            Self {
+                player_id: value.0,
+                report: std::ffi::CStr::from_ptr(value.1)
+                    .to_string_lossy()
+                    .to_string(),
+            }
+        }
+    }
+}
+
+pub struct PlayerModuleListEvent {
+    pub player_id: i32,
+    pub modules: String
+}
+
+impl From<(i32, *const c_char)> for PlayerModuleListEvent {
+    fn from(value: (i32, *const c_char)) -> Self {
+        unsafe {
+            Self {
+                player_id: value.0,
+                modules: std::ffi::CStr::from_ptr(value.1)
+                    .to_string_lossy()
+                    .to_string(),
+            }
+        }
     }
 }
