fae75ae3dbac7eacfaddc281337a4e33c663b3fb
tianxiu2b2t <atianxiua@163.com>
2025-06-29 01:29:49 +0800

    feat: 加了些 player 事件进去，完善基本的东西

--- crates/core/src/callbacks.rs
+++ crates/core/src/callbacks.rs
@@ -1,5 +1,6 @@
 use std::os::raw::c_char;
 
+use crate::py::events::{player::*, server::*};
 use vcmp_bindings::{
     events::player::ClientScriptDataEvent, options::VcmpEntityPool, raw::PluginCallbacks,
 };
@@ -11,32 +12,23 @@ use crate::py::callbacks::CALLBACK;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn on_server_init() -> u8 {
-    logger::event!(logger::Level::DEBUG, "[Rust] Server init callback");
-
-    logger::event!(
-        logger::Level::DEBUG,
-        "server settings {}",
-        vcmp_func().server_version()
-    );
-
-    // println!("gamemode: {}", vcmp_func().get_gamemode());
+    logger::event!(logger::Level::TRACE, "[Rust] Server init callback");
 
     if !CONFIG.get().unwrap().preloader {
         load_script_as_module();
     }
 
-    let _ = CALLBACK.call_func(crate::py::events::server::ServerInitialiseEvent, None);
+    let _ = CALLBACK.call_func(ServerInitialiseEvent, None);
 
     1
 }
 
 #[unsafe(no_mangle)]
 #[allow(clippy::not_unsafe_ptr_arg_deref)]
-pub extern "C" fn on_server_frame(_elapsed_time: f32) {
+pub extern "C" fn on_server_frame(elapsed_time: f32) {
     // println!("[Rust] Server frame callback time: {}", elapsed_time);
+    let _ = CALLBACK.call_func(ServerFrameEvent::from(elapsed_time), None);
 }
-
-// pub fn log_msg_to_vcmp()
 
 ///
 /// # Safety
@@ -65,10 +57,10 @@ pub extern "C" fn on_server_shutdown() {
 pub unsafe extern "C" fn on_entity_pool_change(c_entity_type: i32, entity_id: i32, is_deleted: u8) {
     let entity_type = VcmpEntityPool::from(c_entity_type);
     let deleted = is_deleted != 0;
-    logger::event!(logger::Level::DEBUG, "Entity pool change");
-    logger::event!(logger::Level::DEBUG, "entity type: {entity_type:?}");
-    logger::event!(logger::Level::DEBUG, "entity id: {entity_id}");
-    logger::event!(logger::Level::DEBUG, "deleted: {deleted}");
+    logger::event!(logger::Level::TRACE, "Entity pool change");
+    logger::event!(logger::Level::TRACE, "entity type: {entity_type:?}");
+    logger::event!(logger::Level::TRACE, "entity id: {entity_id}");
+    logger::event!(logger::Level::TRACE, "deleted: {deleted}");
 
     let mut pool = ENTITY_POOL.lock().expect("pool is poisoned");
 
@@ -76,6 +68,41 @@ pub unsafe extern "C" fn on_entity_pool_change(c_entity_type: i32, entity_id: i3
         pool.remove(entity_type, entity_id);
     } else {
         pool.insert(entity_type, entity_id);
+    }
+}
+
+#[unsafe(no_mangle)]
+/// # Safety
+pub unsafe extern "C" fn on_incoming_connection(
+    player_name: *mut ::std::os::raw::c_char,
+    name_buffer_size: usize,
+    user_password: *const ::std::os::raw::c_char,
+    ip_address: *const ::std::os::raw::c_char,
+) -> u8 {
+    CALLBACK.call_func(
+        IncomingConnectionEvent::from((player_name, name_buffer_size, user_password, ip_address)),
+        None,
+    ) as u8
+}
+
+#[unsafe(no_mangle)]
+/// # Safety
+pub unsafe extern "C" fn on_player_connect(player_id: i32) {
+    // first call on_entity_pool_change, maybe vcmp not call on_entity_pool_change?
+    unsafe {
+        on_entity_pool_change(VcmpEntityPool::Player as i32, player_id, 0);
+    }
+
+    let _ = CALLBACK.call_func(PlayerConnectEvent::from(player_id), None);
+}
+
+#[unsafe(no_mangle)]
+/// # Safety
+pub unsafe extern "C" fn on_player_disconnect(player_id: i32, reason: i32) {
+    let _ = CALLBACK.call_func(PlayerDisconnectEvent::from((player_id, reason)), None);
+
+    unsafe {
+        on_entity_pool_change(VcmpEntityPool::Player as i32, player_id, 1);
     }
 }
 
@@ -93,6 +120,12 @@ pub fn init_callbacks(callbacks: &mut PluginCallbacks) {
     callbacks.OnServerInitialise = Some(on_server_init);
     callbacks.OnServerFrame = Some(on_server_frame);
     callbacks.OnServerPerformanceReport = Some(on_server_performance_report);
+    callbacks.OnServerShutdown = Some(on_server_shutdown);
+
+    callbacks.OnIncomingConnection = Some(on_incoming_connection);
+    callbacks.OnPlayerConnect = Some(on_player_connect);
+    callbacks.OnPlayerDisconnect = Some(on_player_disconnect);
+
     callbacks.OnEntityPoolChange = Some(on_entity_pool_change);
 
     callbacks.OnClientScriptData = Some(on_client_script_data);
