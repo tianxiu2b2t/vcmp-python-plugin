6384ba2c0c810acfe2fa10d4eb3e576616f9d53a
tianxiu2b2t <atianxiua@163.com>
2025-06-29 01:33:04 +0800

    feat: 加了个 FinishedException 用于终止 py 执行代码

--- crates/core/src/py/exceptions.rs
+++ crates/core/src/py/exceptions.rs
@@ -1,6 +1,6 @@
 // finishedException
 
-use pyo3::{Bound, PyResult};
+use pyo3::{Bound, PyResult, pymethods};
 use pyo3::{Python, pyclass};
 
 use pyo3::types::{PyModule, PyModuleMethods};
@@ -8,6 +8,14 @@ use pyo3::types::{PyModule, PyModuleMethods};
 #[pyo3(name = "FinishedException")]
 #[derive(Debug, Default)]
 pub struct FinishedException;
+
+#[pymethods]
+impl FinishedException {
+    #[new]
+    fn new(_message: &str) -> Self {
+        Self
+    }
+}
 
 pub fn module_define(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<FinishedException>()?;
