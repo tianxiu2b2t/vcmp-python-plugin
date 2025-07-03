c4067d35c755ba0893322f5774d7eac682c075f6
tianxiu2b2t <atianxiua@163.com>
2025-07-03 23:42:25 +0800

    remove: 准备抛掉 c 了

--- c_src/meta.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef VCMPPLUGIN_TX_META
-#define VCMPPLUGIN_TX_META
-#define LICENSE "MIT"
-#define AUTHOR "TTB-Network"
-#define GITHUB "https://github.com/tianxiu2b2t/vcmp-python-plugin"
-#define GITHUB_REPO "tianxiu2b2t/vcmp-python-plugin"
-#define PLUGIN_NAME "vcmp-python-plugin"
-#define PLUGIN_VERSION "1.2.10.6"
-#endif
