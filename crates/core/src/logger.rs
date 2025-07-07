2b1ce3d5b15314e9a943346dbfd02893f74b00ff
shenjackyuanjie <3695888@qq.com>
2025-07-07 16:44:57 +0800

    it fucking works with OnceLock

--- crates/core/src/logger.rs
+++ crates/core/src/logger.rs
@@ -1,14 +1,24 @@
-use std::io;
-use tracing::{event, Level};
-use tracing_appender::rolling::{RollingFileAppender, Rotation};
-use tracing_subscriber::{fmt::{self, writer::MakeWriterExt}, prelude::*};
+use std::{io, sync::OnceLock};
+use tracing::{Level, event};
+use tracing_appender::{
+    non_blocking::WorkerGuard,
+    rolling::{RollingFileAppender, Rotation},
+};
+use tracing_subscriber::{
+    fmt::{self, writer::MakeWriterExt},
+    prelude::*,
+};
+
+static LOG_GUARD: OnceLock<WorkerGuard> = OnceLock::new();
 
 pub fn init() {
     let max_level = Level::DEBUG; // 硬编码最大日志级别
 
     // 创建按天轮换的文件 appender
     let file_appender = RollingFileAppender::new(Rotation::HOURLY, "logs", "app.log");
-    let (non_blocking_file, _guard) = tracing_appender::non_blocking(file_appender);
+    let (non_blocking_file, guard) = tracing_appender::non_blocking(file_appender);
+
+    let _ = LOG_GUARD.set(guard);
 
     let file = non_blocking_file.with_max_level(max_level);
 
