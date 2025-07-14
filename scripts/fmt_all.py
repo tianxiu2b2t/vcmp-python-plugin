e675fda6ebfe4bfb581bb9b6734a565888c5396e
tianxiu2b2t <atianxiua@163.com>
2025-07-14 13:57:44 +0800

    chore: 格式化代码

---
+++ scripts/fmt_all.py
@@ -0,0 +1,11 @@
+import subprocess
+
+if __name__ == "__main__":
+    try:
+        subprocess.run(["cargo", "+nightly", "fmt", "--all"])
+    except subprocess.CalledProcessError:
+        print("Failed to format all files, use stable")
+        subprocess.run(["cargo", "fmt", "--all"])
+    
+    # py
+    subprocess.run(["black", "src"])
