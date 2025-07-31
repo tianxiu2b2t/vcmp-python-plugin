46b1ddfdcf803fae383bb54af2f349f0f49e424b
tianxiu2b2t <atianxiua@163.com>
2025-07-31 21:58:41 +0800

    remove: 先这样了

--- crates/squirrel_ffi/src/lib.rs
+++ crates/squirrel_ffi/src/lib.rs
@@ -1,26 +1,23 @@
 pub mod sq;
 pub mod sq_ffi;
 
+use tracing::event;
 use vcmp_bindings::{func::PluginMethods, vcmp_func};
 
 use crate::sq::{init_sq_imports, sq_imports};
 
 pub fn init_squirrel() {
-    for i in 0..vcmp_func().get_plugin_count() {
-        println!("Plugin: {}", vcmp_func().get_plugin_info(i as i32).unwrap());
-    }
     let id = match vcmp_func().find_plugin("SQHost2\0") {
         Some(id) => id,
-        None => return,
+        None => {
+            event!(tracing::Level::ERROR, "Failed to find SQHost2 plugin");
+            return;
+        },
     };
     let res = init_sq_imports(vcmp_func().get_plugin_exports(id));
     if res.is_err() {
-        println!("Failed to init squirrel imports: {:?}", res);
+        event!(tracing::Level::ERROR, "Failed to initialize Squirrel FFI");
         return;
     }
-
-    let api = sq_imports().get_api();
-    let vm = sq_imports().get_vm();
-    println!("VM: {:?}", vm);
-    println!("API: {:?}", api);
+    event!(tracing::Level::INFO, "Squirrel FFI initialized");
 }
